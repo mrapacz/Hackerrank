dbaabc30d484501c22677d51cb2be971b73a72ad
mrapacz <maciek.rapacz@gmail.com>
2016-03-24 11:02:35 +0100

    Starting chief hopper problem

---
+++ ChiefHopper.cpp
@@ -0,0 +1,17 @@
+//
+// Created by maciej on 24.03.16.
+//
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> V;
+    for(int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        V.push_back(a);
+    }
+
+    return 0;
+}
