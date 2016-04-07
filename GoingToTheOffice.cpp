c06c5e5f7a7ef370a01fe67ff63a6cf91514be2e
mrapacz <maciek.rapacz@gmail.com>
2016-04-07 18:53:31 +0200

    Bruteforce finished, gonna test it later

--- GoingToTheOffice.cpp
+++ GoingToTheOffice.cpp
@@ -5,61 +5,76 @@
 #include <queue>
 #include <set>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 struct edge {
+    int a;
     int b;
     int w;
 };
+struct vec {
+    int n;
+    int dist;
+};
 
-int tree_size(vector<int> V[], int s) {
-    queue<int> Q;
-    int count = 0;
-    Q.push(s);
-    set<int> seen;
-    while (!Q.empty()) {
-        count++;
-        int current = Q.front();
-        Q.pop();
-        seen.insert(current);
-        for (int i = 0; i < V[current].size(); i++) {
-            int g = V[current][i];
-            if (seen.find(g) == seen.end()) {
-                Q.push(g);
-                seen.insert(g);
-            }
-        }
-    }
-    return count;
-}
-
-void rmv_from_vector(vector<int> &V, int a) {
-    vector<int>::iterator p = find(V.begin(), V.end(), a);
-    V.erase(p);
-    return;
-}
-
-void print_tree(vector<int> V[], int n) {
-    for (int i = 1; i <= n; i++) {
-        cout << "Current i: " << i << endl;
-        for (int j = 0; j < V[i].size(); j++) {
-            cout << "\t " << i << " " << V[i][j] << endl;
-        }
-    }
-}
-int rmv_edge(vector<edge>V[], int a, int b)
-{
+int rmv_edge(vector<edge> &V[], int a, int b) {
     int w = 0;
-    for(int i = 0; i < V[a].size(); i++)
-    {
-        edge current = V[a][i];
-        if(current.b == b){
+    for (vector<edge>::iterator i = V[a].begin(); i < V[a].end(); i++) {
+        edge current = *i;
+        if (current.b == b) {
             w = current.w;
-            //todo: sklepac dijkstre
+            V[a].erase(i++);
         }
     }
     return w;
 }
+
+bool operator<(vec a, vec b) {
+    return a.dist <= b.dist;
+}
+
+int dijkstra(vector<edge> V[], int s, int d) {
+
+    map<int, int> M;
+    M[s] = 0;
+    priority_queue<vec> Q;
+    vec current;
+
+    current.n = s;
+    current.dist = 0;
+    Q.push(current);
+
+    while (!Q.empty()) {
+        current = Q.top();
+        Q.pop();
+        for (int i = 0; i < V[current.n].size(); i++) {
+            int next = V[current.n][i].b;
+            int weight = V[current.n][i].w;
+            if (M.find(next) == M.end() or M[current.n] + weight < M[next]) {
+                M[next] = M[current.n] + weight;
+                vec x;
+                x.n = next;
+                x.dist = M[next];
+                Q.push(x);
+            }
+        }
+    }
+    if (M.find(d) == M.end()) return -1;
+    else return M[d];
+}
+
+void add_edge(vector<edge> &V[], int a, int b, int w) {
+    edge e;
+    e.a = a;
+    e.b = b;
+    e.w = w;
+    V[a].push_back(e);
+    swap(e.a, e.b);
+    V[b].push_back(a);
+    return;
+}
+
 int main() {
     vector<edge> V[200000];
     int n, m;
@@ -68,11 +83,7 @@ int main() {
         int a, b, w;
         edge e;
         cin >> a >> b >> w;
-        e.b = b;
-        e.w = w;
-        V[a].push_back(e);
-        e.b = a;
-        V[b].push_back(e);
+        add_edge(V, a, b, w);
     }
     int s, d;
     cin >> s >> d;
@@ -83,8 +94,8 @@ int main() {
         cin >> a >> b;
         int w = rmv_edge(V, a, b);
         rmv_edge(V, b, a);
-        cout << dijkstra(V,s,d);
-
+        cout << dijkstra(V, s, d);
+        add_edge(V, a, b, w);
     }
 
     //print_tree(V, n);
