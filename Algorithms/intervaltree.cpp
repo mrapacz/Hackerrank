6e3ac9f1e205641da24edb7bd2030e112e16e82a
mrapacz <maciek.rapacz@gmail.com>
2016-04-30 13:22:31 +0200

    interval tree to be written

--- Algorithms/intervaltree.cpp
+++ Algorithms/intervaltree.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+int get_tree_size(int n) {
+    return 1 << (int(log2(n - 1)) + 2);
+}
+
 int main() {
+    int n;
+    int T[1 << (int(log2(n - 1)) + 2)];
+
+
     return 0;
 }
