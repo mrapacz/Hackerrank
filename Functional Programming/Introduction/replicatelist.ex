b370291f706e8caab8bcd2ffa45bd7f51380c8b6
Maciej Rapacz <maciek.rapacz@gmail.com>
2016-11-08 14:51:22 +0100

    Fix bad tab

--- Functional Programming/Introduction/replicatelist.ex
+++ Functional Programming/Introduction/replicatelist.ex
@@ -1,11 +1,11 @@
 defmodule Replicatelist do
     def replicate(n) do
-		case IO.gets "" do
-			:eof -> :ok
-			element -> 
-				String.duplicate(element, n) |> IO.write
-				replicate n
-		end
+	case IO.gets "" do
+		eof -> :ok
+		element -> 
+			String.duplicate(element, n) |> IO.write
+			replicate n
+	end
     end
 end
 n = IO.gets("") |> String.strip |> String.to_integer
