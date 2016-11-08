4c5e6526fa2d80c84664396966bc0957674b6be6
Maciej Rapacz <maciek.rapacz@gmail.com>
2016-11-08 14:48:22 +0100

    Move missing numbers to correct directory and add elixir solution to replicatelist

---
+++ Functional Programming/Introduction/replicatelist.ex
@@ -0,0 +1,12 @@
+defmodule Replicatelist do
+    def replicate(n) do
+		case IO.gets "" do
+			:eof -> :ok
+			element -> 
+				String.duplicate(element, n) |> IO.write
+				replicate n
+		end
+    end
+end
+n = IO.gets("") |> String.strip |> String.to_integer
+Replicatelist.replicate n
